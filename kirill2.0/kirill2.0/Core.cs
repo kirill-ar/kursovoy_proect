534fa7579d754acda6acb599d5bce8ba47433214
Name <email>
2021-06-04 14:19:03 +0300

    курсач

---
+++ kirill2.0/kirill2.0/Core.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kirill2._0
+{
+    class Core
+    {
+        public static bakery_kpEntities DB = new bakery_kpEntities();
+    }
+
+
+}
---
+++ kirill2.0/kirill2.0/CreateWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace kirill2._0
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateWindow.xaml
+    /// </summary>
+    public partial class CreateWindow : Window, INotifyPropertyChanged
+    {
+        //public List<ProductType> WorkList { get; set; }
+
+        public Product CurrentService { get; set; }
+
+        public string WindowName
+        {
+            get
+            {
+                return CurrentService.Id == 0 ? "Новый сотрудник" : "Редактирование сотрудника";
+            }
+        }
+
+        public CreateWindow(Product prod)
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            CurrentService = prod;
+            //RoleList = Core.DB.Role.ToList();
+            //WorkList = Core.DB.ProductType.ToList();
+        }
+
+        private void GetImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog GetImageDialog = new OpenFileDialog();
+            // задаем фильтр для выбираемых файлов
+            // до символа "|" идет произвольный текст, а после него шаблоны файлов раздеренные точкой с запятой
+            GetImageDialog.Filter = "Файлы изображений: (*.png, *.jpg)|*.png;*.jpg";
+            // чтобы не искать по всему диску задаем начальный каталог
+            GetImageDialog.InitialDirectory = Environment.CurrentDirectory;
+            if (GetImageDialog.ShowDialog() == true)
+            {
+                // перед присвоением пути к картинке обрезаем начало строки, т.к. диалог возвращает полный путь
+                // (тут конечно еще надо проверить есть ли в начале Environment.CurrentDirectory)
+                CurrentService.Photo = GetImageDialog.FileName.Substring(Environment.CurrentDirectory.Length + 1);
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("CurrentService"));
+                }
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentService.Price <= 0)
+            {
+                MessageBox.Show("Стоимость услуги должна быть больше ноля");
+                return;
+            }
+
+
+            // если запись новая, то добавляем ее в список
+            if (CurrentService.Id == 0)
+                Core.DB.Product.Add(CurrentService);
+
+            // сохранение в БД
+            try
+            {
+                Core.DB.SaveChanges();
+            }
+            catch
+            {
+            }
+            DialogResult = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
